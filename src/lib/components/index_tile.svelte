37bdf99d005b456519d7cbd14d380e11f27604cf
Gjorgdy <jordy.reins@proton.me>
2026-07-16 01:23:17 +0200

    ALT: positioning

--- src/lib/components/index_tile.svelte
+++ src/lib/components/index_tile.svelte
@@ -4,12 +4,12 @@
   const { class: className, imgClass, title, subtitle, thumbnail, href, saturated }: { class?: string; imgClass?: string; title: string; subtitle?: string; thumbnail: string; href: ResolvedPathname; saturated?: boolean } = $props();
 </script>
 
-<a href={href} class="w-full h-full group flex flex-col gap-2 max-h-80 {className}">
-    <img src={thumbnail} alt={title} class="w-full h-full object-cover {saturated ? 'group-focus:saturate-30 group-hover:saturate-30' : 'saturate-30 group-focus:saturate-100 group-hover:saturate-100'} transition-all {imgClass}"/>
+<a href={href} class="w-full h-full group flex flex-col gap-2 {className}">
+    <img src={thumbnail} alt={title} class="w-full h-full object-cover {saturated ? 'group-focus:saturate-30 group-hover:saturate-30' : 'saturate-30 group-focus:saturate-100 group-hover:saturate-100'} transition-all max-h-40 md:max-h-80 {imgClass}"/>
     <span class="flex flex-row justify-between items-end">
         <span class="underline group-hover:text-gray-600 dark:group-hover:text-gray-300 transition-colors not-md:text-sm">{title}</span>
         {#if subtitle}
-            <span class="text-gray-600 dark:text-white/40 text-sm italic opacity-0 group-hover:opacity-100 -translate-y-1 group-hover:translate-0 transition-all">
+            <span class="not-md:hidden overflow-hidden text-gray-600 dark:text-white/40 text-sm italic md:opacity-0 group-hover:opacity-100 -translate-y-1 group-hover:translate-0 transition-all">
                 {subtitle}
             </span>
         {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
 </script>
 
 <div id="bg" class="min-h-dvh bg-blue-50 text-gray-900 dark:bg-black dark:text-white flex flex-col items-center">
-    <div class="w-full max-w-400 h-full relative">
+    <div class="w-full max-w-400 h-full relative pb-4">
         <div id="title" class="flex flex-col md:gap-4 md:items-center p-3 py-4 md:py-20">
             <div id="bar" class="flex flex-row justify-between items-center">
                 <h1 class="text-4xl md:text-8xl font-bold tracking-widest">JORDY REINS</h1>
@@ -37,18 +37,18 @@
                 <TypeWriter texts={hobbies} afterTyped={{wait: 5000}} deleteSpeed={80} typeSpeed={80} />
             </span>
         </div>
-        <h2 class="p-2 md:p-8 md:pt-0 text-xl text-gray-500">// Personal</h2>
+        <h2 class="p-2 pb-4 pt-0 md:p-8 md:pt-0 text-xl text-gray-500">// Personal</h2>
         <div id="me" class="w-full h-full grid grid-cols-2 lg:grid-cols-4 gap-4 px-2 md:px-8">
             <IndexTile class="col-span-2" title={m.about_me()} subtitle={m.gaming_at_4()} thumbnail={aboutMeThumbnail} href={resolve("/about")} {saturated}/>
             <IndexTile imgClass="object-left" title={m.experiences()} subtitle={m.current_school()} thumbnail={experiencesThumbnail} href={resolve("/experiences")} {saturated}/>
             <IndexTile title={m.contact_me()} subtitle={m.contact_me_subtitle()} thumbnail={contactThumbnail} href={resolve("/contact")} {saturated}/>
         </div>
-        <h2 class="p-2 md:p-8 text-xl text-gray-500">// Projects</h2>
+        <h2 class="p-2 py-4 md:p-8 text-xl text-gray-500">// Projects</h2>
         <div id="projects" class="w-full h-full grid grid-cols-2 lg:grid-cols-4 gap-4 px-2 md:px-8">
             <IndexTile title="Helix (Discord) Bot" subtitle={m.helix_bot_subtitle()} thumbnail={helixBotThumbnail} href={resolve("/projects/helix-bot")} {saturated}/>
             <IndexTile title="Helix Survival Server" subtitle={m.helix_subtitle()} thumbnail={helixThumbnail} href={resolve("/projects/helix-survival")} {saturated}/>
             <IndexTile title="Symphonic SMP Website" subtitle={m.symphonic_subtitle()} thumbnail={symphonicThumbnail} href={resolve("/projects/symphonic-smp")} {saturated}/>
         </div>
-        <!-- <h2 class="p-2 md:p-8 text-xl text-gray-500">// Tools</h2> -->
+        <!-- <h2 class="p-2 py-4 md:p-8 text-xl text-gray-500">// Tools</h2> -->
     </div>
 </div>
