3079af2c1f7677e3a5668e77b9fb360d11924423
Gjorgdy <jordy.reins@proton.me>
2026-07-22 15:21:35 +0200

    ALT: last updated

--- src/lib/components/last_updated.svelte
+++ src/lib/components/last_updated.svelte
@@ -3,4 +3,4 @@
     import { m } from '$lib/paraglide/messages.js';
 </script>
 
-<div class={className}>{`{"${m.last_updated_sk()}": "2026-07-21"}`}</div>
+<div class={className}>{`{"${m.last_updated_sk()}": "2026-07-22"}`}</div>
